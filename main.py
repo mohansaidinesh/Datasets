91e0a929578ac794bce5bc02db6beab6b4f110d2
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2024-03-24 12:13:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,250 @@
 import streamlit as st
-from PIL import Image
-from keras.preprocessing.image import load_img,img_to_array
+import requests
+import pyowm
+import pandas as pd
 import numpy as np
-from keras.models import load_model
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics
+import warnings
+warnings.filterwarnings('ignore')
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import warnings
+warnings.filterwarnings('ignore')
+owm = pyowm.OWM('11081b639d8ada3e97fc695bcf6ddb20')
+from PIL import Image
+import time
+navigation_menu = ["Home", "Weather","Crop Recommendation","Fertilizer"]
+selected = st.sidebar.selectbox("Navigation", navigation_menu)
+if selected=='Home':
+        st.markdown(f"<h1 style='text-align: center;font-size:60px;color:#33ccff;'>Agriculture</h1>", unsafe_allow_html=True)
+if selected=='Weather':
+        st.markdown(f"<h1 style='text-align: center; color:skyblue;'>Weather</h1>", unsafe_allow_html=True)
+        id = st.text_input("Enter City")
+        try:
+            mgr = owm.weather_manager()
+            observation = mgr.weather_at_place(id)
+            weather = observation.weather
+            t1 = weather.temperature('celsius')['temp']
+            h1 = weather.humidity
+            w1 = weather.wind()
+            p1=weather.pressure['press']
+            num_weekdays = 5
+            count_weekdays = 0
+            weekday_names = []
+            now = time.time()
+            now1 = time.localtime()
+            us_date = time.strftime("%m/%d/%Y", now1)
+            while count_weekdays < num_weekdays:
+                now += 86400
+                local_time = time.localtime(now)
+                weekday = local_time.tm_wday
+                wn = time.strftime("%a", local_time)
+                if count_weekdays!=5:
+                    count_weekdays += 1
+                    weekday_names.append(time.strftime("%a", local_time))
+            col1, col2,col3,col4= st.columns([2,8,5,3])
+            col1.image('download (1).png', width=75)
+            with col4:
+                pass
+            with col2:
+                st.markdown(f"<h4 style='color:red;'>{t1}°C</h4>", unsafe_allow_html=True)
+            col1,col2,col3,col4= st.columns([5,8,5,5])
+            with col1:
+                st.markdown(f"<p>{'Humidity :  '}{h1}%</p>", unsafe_allow_html=True)
+            with col1:
+                st.markdown(f"<p>{'Pressure :  '}{' '}{p1}hPa</p>", unsafe_allow_html=True)
+            with col1:
+                st.markdown(f"<p>{'Wind Speed:  '}{w1['speed']}hPa</p>", unsafe_allow_html=True)  
+            col1, col2,col3,col4,col5= st.columns([4,4,4,4,4])
+            forecaster = mgr.forecast_at_place(id, '3h', limit=40)
 
-model = load_model('BC.h5',compile=False)
-lab = {0: 'AFRICAN CROWNED CRANE', 1: 'AFRICAN FIREFINCH', 2: 'ALBATROSS', 3: 'ALEXANDRINE PARAKEET', 4: 'AMERICAN AVOCET', 5: 'AMERICAN BITTERN', 6: 'AMERICAN COOT', 7: 'AMERICAN GOLDFINCH', 8: 'AMERICAN KESTREL', 9: 'AMERICAN PIPIT', 10: 'AMERICAN REDSTART', 11: 'ANHINGA', 12: 'ANNAS HUMMINGBIRD', 13: 'ANTBIRD', 14: 'ARARIPE MANAKIN', 15: 'ASIAN CRESTED IBIS', 16: 'BALD EAGLE', 17: 'BALI STARLING', 18: 'BALTIMORE ORIOLE', 19: 'BANANAQUIT', 20: 'BANDED BROADBILL', 21: 'BAR-TAILED GODWIT', 22: 'BARN OWL', 23: 'BARN SWALLOW', 24: 'BARRED PUFFBIRD', 25: 'BAY-BREASTED WARBLER', 26: 'BEARDED BARBET', 27: 'BEARDED REEDLING', 28: 'BELTED KINGFISHER', 29: 'BIRD OF PARADISE', 30: 'BLACK & YELLOW bROADBILL', 31: 'BLACK FRANCOLIN', 32: 'BLACK SKIMMER', 33: 'BLACK SWAN', 34: 'BLACK TAIL CRAKE', 35: 'BLACK THROATED BUSHTIT', 36: 'BLACK THROATED WARBLER', 37: 'BLACK VULTURE', 38: 'BLACK-CAPPED CHICKADEE', 39: 'BLACK-NECKED GREBE', 40: 'BLACK-THROATED SPARROW', 41: 'BLACKBURNIAM WARBLER', 42: 'BLUE GROUSE', 43: 'BLUE HERON', 44: 'BOBOLINK', 45: 'BORNEAN BRISTLEHEAD', 46: 'BORNEAN LEAFBIRD', 47: 'BROWN NOODY', 48: 'BROWN THRASHER', 49: 'BULWERS PHEASANT', 50: 'CACTUS WREN', 51: 'CALIFORNIA CONDOR', 52: 'CALIFORNIA GULL', 53: 'CALIFORNIA QUAIL', 54: 'CANARY', 55: 'CAPE MAY WARBLER', 56: 'CAPUCHINBIRD', 57: 'CARMINE BEE-EATER', 58: 'CASPIAN TERN', 59: 'CASSOWARY', 60: 'CEDAR WAXWING', 61: 'CHARA DE COLLAR', 62: 'CHIPPING SPARROW', 63: 'CHUKAR PARTRIDGE', 64: 'CINNAMON TEAL', 65: 'COCK OF THE  ROCK', 66: 'COCKATOO', 67: 'COMMON FIRECREST', 68: 'COMMON GRACKLE', 69: 'COMMON HOUSE MARTIN', 70: 'COMMON LOON', 71: 'COMMON POORWILL', 72: 'COMMON STARLING', 73: 'COUCHS KINGBIRD', 74: 'CRESTED AUKLET', 75: 'CRESTED CARACARA', 76: 'CRESTED NUTHATCH', 77: 'CROW', 78: 'CROWNED PIGEON', 79: 'CUBAN TODY', 80: 'CURL CRESTED ARACURI', 81: 'D-ARNAUDS BARBET', 82: 'DARK EYED JUNCO', 83: 'DOUBLE BARRED FINCH', 84: 'DOWNY WOODPECKER', 85: 'EASTERN BLUEBIRD', 86: 'EASTERN MEADOWLARK', 87: 'EASTERN ROSELLA', 88: 'EASTERN TOWEE', 89: 'ELEGANT TROGON', 90: 'ELLIOTS  PHEASANT', 91: 'EMPEROR PENGUIN', 92: 'EMU', 93: 'ENGGANO MYNA', 94: 'EURASIAN GOLDEN ORIOLE', 95: 'EURASIAN MAGPIE', 96: 'EVENING GROSBEAK', 97: 'FIRE TAILLED MYZORNIS', 98: 'FLAME TANAGER', 99: 'FLAMINGO', 100: 'FRIGATE', 101: 'GAMBELS QUAIL', 102: 'GANG GANG COCKATOO', 103: 'GILA WOODPECKER', 104: 'GILDED FLICKER', 105: 'GLOSSY IBIS', 106: 'GO AWAY BIRD', 107: 'GOLD WING WARBLER', 108: 'GOLDEN CHEEKED WARBLER', 109: 'GOLDEN CHLOROPHONIA', 110: 'GOLDEN EAGLE', 111: 'GOLDEN PHEASANT', 112: 'GOLDEN PIPIT', 113: 'GOULDIAN FINCH', 114: 'GRAY CATBIRD', 115: 'GRAY PARTRIDGE', 116: 'GREAT POTOO', 117: 'GREATOR SAGE GROUSE', 118: 'GREEN JAY', 119: 'GREEN MAGPIE', 120: 'GREY PLOVER', 121: 'GUINEA TURACO', 122: 'GUINEAFOWL', 123: 'GYRFALCON', 124: 'HARPY EAGLE', 125: 'HAWAIIAN GOOSE', 126: 'HELMET VANGA', 127: 'HIMALAYAN MONAL', 128: 'HOATZIN', 129: 'HOODED MERGANSER', 130: 'HOOPOES', 131: 'HORNBILL', 132: 'HORNED GUAN', 133: 'HORNED SUNGEM', 134: 'HOUSE FINCH', 135: 'HOUSE SPARROW', 136: 'IMPERIAL SHAQ', 137: 'INCA TERN', 138: 'INDIAN BUSTARD', 139: 'INDIAN PITTA', 140: 'INDIGO BUNTING', 141: 'JABIRU', 142: 'JAVA SPARROW', 143: 'KAKAPO', 144: 'KILLDEAR', 145: 'KING VULTURE', 146: 'KIWI', 147: 'KOOKABURRA', 148: 'LARK BUNTING', 149: 'LEARS MACAW', 150: 'LILAC ROLLER', 151: 'LONG-EARED OWL', 152: 'MAGPIE GOOSE', 153: 'MALABAR HORNBILL', 154: 'MALACHITE KINGFISHER', 155: 'MALEO', 156: 'MALLARD DUCK', 157: 'MANDRIN DUCK', 158: 'MARABOU STORK', 159: 'MASKED BOOBY', 160: 'MASKED LAPWING', 161: 'MIKADO  PHEASANT', 162: 'MOURNING DOVE', 163: 'MYNA', 164: 'NICOBAR PIGEON', 165: 'NOISY FRIARBIRD', 166: 'NORTHERN BALD IBIS', 167: 'NORTHERN CARDINAL', 168: 'NORTHERN FLICKER', 169: 'NORTHERN GANNET', 170: 'NORTHERN GOSHAWK', 171: 'NORTHERN JACANA', 172: 'NORTHERN MOCKINGBIRD', 173: 'NORTHERN PARULA', 174: 'NORTHERN RED BISHOP', 175: 'NORTHERN SHOVELER', 176: 'OCELLATED TURKEY', 177: 'OKINAWA RAIL', 178: 'OSPREY', 179: 'OSTRICH', 180: 'OYSTER CATCHER', 181: 'PAINTED BUNTIG', 182: 'PALILA', 183: 'PARADISE TANAGER', 184: 'PARUS MAJOR', 185: 'PEACOCK', 186: 'PELICAN', 187: 'PEREGRINE FALCON', 188: 'PHILIPPINE EAGLE', 189: 'PINK ROBIN', 190: 'PUFFIN', 191: 'PURPLE FINCH', 192: 'PURPLE GALLINULE', 193: 'PURPLE MARTIN', 194: 'PURPLE SWAMPHEN', 195: 'QUETZAL', 196: 'RAINBOW LORIKEET', 197: 'RAZORBILL', 198: 'RED BEARDED BEE EATER', 199: 'RED BELLIED PITTA', 200: 'RED BROWED FINCH', 201: 'RED FACED CORMORANT', 202: 'RED FACED WARBLER', 203: 'RED HEADED DUCK', 204: 'RED HEADED WOODPECKER', 205: 'RED HONEY CREEPER', 206: 'RED TAILED THRUSH', 207: 'RED WINGED BLACKBIRD', 208: 'RED WISKERED BULBUL', 209: 'REGENT BOWERBIRD', 210: 'RING-NECKED PHEASANT', 211: 'ROADRUNNER', 212: 'ROBIN', 213: 'ROCK DOVE', 214: 'ROSY FACED LOVEBIRD', 215: 'ROUGH LEG BUZZARD', 216: 'ROYAL FLYCATCHER', 217: 'RUBY THROATED HUMMINGBIRD', 218: 'RUFOUS KINGFISHER', 219: 'RUFUOS MOTMOT', 220: 'SAMATRAN THRUSH', 221: 'SAND MARTIN', 222: 'SCARLET IBIS', 223: 'SCARLET MACAW', 224: 'SHOEBILL', 225: 'SHORT BILLED DOWITCHER', 226: 'SMITHS LONGSPUR', 227: 'SNOWY EGRET', 228: 'SNOWY OWL', 229: 'SORA', 230: 'SPANGLED COTINGA', 231: 'SPLENDID WREN', 232: 'SPOON BILED SANDPIPER', 233: 'SPOONBILL', 234: 'SRI LANKA BLUE MAGPIE', 235: 'STEAMER DUCK', 236: 'STORK BILLED KINGFISHER', 237: 'STRAWBERRY FINCH', 238: 'STRIPPED SWALLOW', 239: 'SUPERB STARLING', 240: 'SWINHOES PHEASANT', 241: 'TAIWAN MAGPIE', 242: 'TAKAHE', 243: 'TASMANIAN HEN', 244: 'TEAL DUCK', 245: 'TIT MOUSE', 246: 'TOUCHAN', 247: 'TOWNSENDS WARBLER', 248: 'TREE SWALLOW', 249: 'TRUMPTER SWAN', 250: 'TURKEY VULTURE', 251: 'TURQUOISE MOTMOT', 252: 'UMBRELLA BIRD', 253: 'VARIED THRUSH', 254: 'VENEZUELIAN TROUPIAL', 255: 'VERMILION FLYCATHER', 256: 'VICTORIA CROWNED PIGEON', 257: 'VIOLET GREEN SWALLOW', 258: 'VULTURINE GUINEAFOWL', 259: 'WATTLED CURASSOW', 260: 'WHIMBREL', 261: 'WHITE CHEEKED TURACO', 262: 'WHITE NECKED RAVEN', 263: 'WHITE TAILED TROPIC', 264: 'WILD TURKEY', 265: 'WILSONS BIRD OF PARADISE', 266: 'WOOD DUCK', 267: 'YELLOW BELLIED FLOWERPECKER', 268: 'YELLOW CACIQUE', 269: 'YELLOW HEADED BLACKBIRD'}
+            c=0
+            l=[]
+            for weather in forecaster.forecast:
+                temperature = weather.temperature('celsius')['temp']
+                c+=1
+                if c==8 or c==16 or c==24 or c==32 or c==40:
+                    l.append(temperature)
+            with col1:
+                st.markdown(f"<h4 style='color:#EE82EE	';>{weekday_names[0]}</h4>", unsafe_allow_html=True)
+                st.markdown(f"<p>{l[0]}°C</p>", unsafe_allow_html=True)
+            with col2:
+                st.markdown(f"<h4 style='color:blue';>{weekday_names[1]}</h4>", unsafe_allow_html=True)
+                st.markdown(f"<p>{l[1]}°C</p>", unsafe_allow_html=True)
+            with col3:
+                st.markdown(f"<h4 style='color:green';>{weekday_names[2]}</h4>", unsafe_allow_html=True)
+                st.markdown(f"<p>{l[2]}°C</p>", unsafe_allow_html=True)
+            with col4:
+                st.markdown(f"<h4 style='color:orange';>{weekday_names[3]}</h4>", unsafe_allow_html=True)
+                st.markdown(f"<p>{l[3]}°C</p>", unsafe_allow_html=True)
+            with col5:
+                st.markdown(f"<h4 style='color:red';>{weekday_names[4]}</h4>", unsafe_allow_html=True)
+                st.markdown(f"<p>{l[4]}°C</p>", unsafe_allow_html=True)
+        except:
+            pass
+if selected=='Crop Recommendation':
+    st.markdown(f"<h1 style='text-align: center; color:red;'>Crop Recomendation</h1>", unsafe_allow_html=True)
+    df=pd.read_csv("Crop_recommendation.csv")
+    features = df[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
+    target = df['label']
+    labels = df['label']
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(features,target,test_size = 0.2,random_state =2)
+    RF = RandomForestClassifier(n_estimators=25, random_state=42)
+    RF.fit(Xtrain,Ytrain)
+    col1, col2,col3= st.columns([5,5,5])
+    with col1:
+        a=st.number_input('Enter N')
+    with col2:
+        b=st.number_input('Enter P')
+    with col3:
+        c1=st.number_input('Enter K')
+    col1, col2,col3,col4= st.columns([5,5,5,5])
+    with col1:
+        d=st.number_input('Temperature °C')
+    with col2:
+        e=st.number_input('Humidity %')
+    with col3:
+        f=st.number_input('pH')
+    with col4:
+        g=st.number_input('Rainfall mm')
+    data = np.array([[a,b,c1,d,e,f,g]])
+    prediction = RF.predict(data)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write(' ')
+    with col2:
+        if prediction[0]=='apple':
+            st.image("images/apple.jpg")
+        if prediction[0]=='banana':
+            st.image("images/banana.jpg")
+        if prediction[0]=='blackgram':
+            st.image("images/blackgram.jpg")
+        if prediction[0]=='chickpea':
+            st.image("images/chickpea.jpg")
+        if prediction[0]=='coconut':
+            st.image("images/coconut.jpg")
+        if prediction[0]=='coffee':
+            st.image("images/coffee.jpg")
+        if prediction[0]=='cotton':
+            st.image("images/cotton.jpg")
+        if prediction[0]=='grapes':
+            st.image("images/grapes.jpg")
+        if prediction[0]=='jute':
+            st.image("images/jute.jpg")
+        if prediction[0]=='kidneybeans':
+            st.image("images/kidneybeans.jpg")
+        if prediction[0]=='lentil':
+            st.image("images/lentil.jpg")
+        if prediction[0]=='maize':
+            st.image("images/maize.jpg")
+        if prediction[0]=='mango':
+            st.image("images/mango.jpg")
+        if prediction[0]=='mothbeans':
+            st.image("images/mothbeans.jpg")
+        if prediction[0]=='mungbean':
+            st.image("images/mungbeans.jpg")
+        if prediction[0]=='muskmelon':
+            st.image("images/muskmelon.jpg")
+        if prediction[0]=='orange':
+            st.image("images/orange.jpg")
+        if prediction[0]=='papaya':
+            st.image("images/papaya.jpg")
+        if prediction[0]=='pomegranate':
+            st.image("images/pomegranate.jpg")
+        if prediction[0]=='pigeonpeas':
+            st.image("images/pigeonpeas.jpg")
+        if prediction[0]=='rice':
+            st.image("images/rice.jpg")
+        if prediction[0]=='watermelon':
+            st.image("images/watermelon.jpg")
+    with col3:
+        st.write(' ')
+if selected=='Fertilizer':
+    st.markdown(f"<h1 style='text-align: center; color:blue;'>Fertilizer Prediction</h1>", unsafe_allow_html=True)
+    data = pd.read_csv('Fertilizer Prediction.csv')
+    data.rename(columns={'Humidity ':'Humidity','Soil Type':'Soil_Type','Crop Type':'Crop_Type','Fertilizer Name':'Fertilizer'},inplace=True)
+    from sklearn.preprocessing import LabelEncoder
+    encode_soil = LabelEncoder()
+    data.Soil_Type = encode_soil.fit_transform(data.Soil_Type)
+    Soil_Type = pd.DataFrame(zip(encode_soil.classes_,encode_soil.transform(encode_soil.classes_)),columns=['Original','Encoded'])
+    Soil_Type = Soil_Type.set_index('Original')
+    encode_crop = LabelEncoder()
+    data.Crop_Type = encode_crop.fit_transform(data.Crop_Type)
+    Crop_Type = pd.DataFrame(zip(encode_crop.classes_,encode_crop.transform(encode_crop.classes_)),columns=['Original','Encoded'])
+    Crop_Type = Crop_Type.set_index('Original')
+    encode_ferti = LabelEncoder()
+    data.Fertilizer = encode_ferti.fit_transform(data.Fertilizer)
+    Fertilizer = pd.DataFrame(zip(encode_ferti.classes_,encode_ferti.transform(encode_ferti.classes_)),columns=['Original','Encoded'])
+    Fertilizer = Fertilizer.set_index('Original')
+    from sklearn.model_selection import train_test_split
+    x_train, x_test, y_train, y_test = train_test_split(data.drop('Fertilizer',axis=1),data.Fertilizer,test_size=0.2,random_state=1)
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+    from sklearn.ensemble import RandomForestClassifier
+    soil=['Black','Clayey','Loamy','Red','Sandy']
+    crop=['Barley','Cotton','Ground Nuts','Maize','Millets','Oil seeds','Paddy','Pulses','Sugarcane','Tobacco','Wheat']
+    fert=['10-26-26','14-35-14','17-17-17','20-20','28-28','DAP','Urea']
+    rand = RandomForestClassifier(n_estimators=30,random_state=42)
+    pred_rand = rand.fit(x_train,y_train).predict(x_test)
+    col1, col2,col3= st.columns([5,5,5])
+    with col1:
+        a=st.number_input('Temperature °C')
+    with col2:
+        b=st.number_input('Humidity %')
+    with col3:
+        c=st.number_input('Moisture')
+    col1,col2= st.columns([5,5])
+    with col1:
+        d=st.selectbox('Soil Type',('Black','Clayey','Loamy','Red','Sandy'))
+    with col2:
+        e=st.selectbox('Crop Type',('Barley','Cotton','Ground Nuts','Maize','Millets','Oil seeds','Paddy','Pulses','Sugarcane','Tobacco','Wheat'))
+    col1, col2,col3= st.columns([5,5,5])
+    with col1:
+        f=st.number_input('Enter N')
+    with col2:
+        g=st.number_input('Enter P')
+    with col3:
+        h=st.number_input('Enter K')
+    data = np.array([[a,b,c,soil.index(d),crop.index(e),f,g,h]])
+    prediction = rand.predict(data)
+    res=fert[prediction[0]]
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write(' ')
+    with col2:
+        if res=='10-26-26':
+            st.image("images/10-26-26.jpg")
+        if res=='14-35-14':
+            st.image("images/14-35-14.jpg")
+        if res=='17-17-17':
+            st.image("images/17-17-17.jpg")
+        if res=='20-20':
+            st.image("images/20-20.jpg")
+        if res=='28-28':
+            st.image("images/28-28.jpg")
+        if res=='DAP':
+            st.image("images/DAP.jpg")
+        if res=='Urea':
+            st.image("images/Urea.jpg")
+    with col3:
+        st.write(' ')
 
-def processed_img(img_path):
-    img=load_img(img_path,target_size=(224,224,3))
-    img=img_to_array(img)
-    img=img/255
-    img=np.expand_dims(img,[0])
-    answer=model.predict(img)
-    y_class = answer.argmax(axis=-1)
-    print(y_class)
-    y = " ".join(str(x) for x in y_class)
-    y = int(y)
-    res = lab[y]
-    print(res)
-    return res
-
-def run():
-    img1 = Image.open('logo1.png')
-    img1 = img1.resize((350,350))
-    st.image(img1,use_column_width=False)
-    st.title("Birds Species Classification")
-    st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Data is based "270 Bird Species also see 70 Sports Dataset"</h4>''',
-                unsafe_allow_html=True)
-
-    img_file = st.file_uploader("Choose an Image of Bird", type=["jpg", "png"])
-    if img_file is not None:
-        st.image(img_file)
-        if st.button("Predict"):
-            result = processed_img(img_file)
-            st.success("Predicted Bird is: "+result)
-run()
+    
