7bc114df1a612cfef3a5fb6ca41c9b33e982c8bc
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2024-06-30 08:56:57 +0530

    Add files via upload

---
+++ Stall.cs
@@ -0,0 +1,19 @@
+﻿namespace MvcMovie.Models
+{
+    public class Stall
+    {
+        private string _name;
+        private string _type;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Type { get { return _type; } set { _type = value; } }
+
+        public Stall() { }
+
+        public Stall(string name, string type)
+        {
+            _name = name;
+            _type = type;
+        }
+    }
+}
